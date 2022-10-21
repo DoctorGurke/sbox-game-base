de24165a92b9e0d12e8704bd14b3c7af80a45ea8
Doctor Gurke <docgurk@gmail.com>
2022-10-21 13:23:52 +0200

    Debug system with Log.Debug

---
+++ code/Systems/Debug/Log.Debug.cs
@@ -0,0 +1,11 @@
+﻿namespace GameBase.Systems.Debug;
+
+public static class DebugLogX
+{
+	public static void Debug( this Logger log, object message )
+	{
+#if DEBUG
+		Log.Info( $"[{(Host.IsServer ? "SV" : "CL")}] {message}" );
+#endif
+	}
+}
